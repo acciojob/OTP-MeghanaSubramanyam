8192bdce558fb4c07407f7023e1eb16dad1dd689
MeghanaSubramanyam <152699209+MeghanaSubramanyam@users.noreply.github.com>
2024-09-20 19:56:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 
+// const inputs = document.querySelectorAll('.code');
+
+// inputs.forEach((input, index) => {
+//     input.addEventListener('input', () => {
+//         if (input.value.length === 1 && index < inputs.length - 1) {
+//             inputs[index + 1].focus();
+//         }
+//     });
+
+//     input.addEventListener('keydown', (event) => {
+//         if (event.key === 'Backspace' && input.value === "") {
+//             if (index > 0) {
+//                 inputs[index - 1].focused();
+//                 inputs[index - 1].value = '';
+//             }
+//         }
+//     });
+// });
 const inputs = document.querySelectorAll('.code');
 
 inputs.forEach((input, index) => {
@@ -11,8 +29,7 @@ inputs.forEach((input, index) => {
     input.addEventListener('keydown', (event) => {
         if (event.key === 'Backspace' && input.value === "") {
             if (index > 0) {
-                inputs[index - 1].focused();
-                inputs[index - 1].value = '';
+                inputs[index - 1].focus();
             }
         }
     });
